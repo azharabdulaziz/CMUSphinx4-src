d8126166b280cdc064224555ee4404aba4b0f3c9
Azhar <azhar.abdulaziz@gmail.com>
2017-11-14 13:21:02 -0500

    Adding BatchCombine
    Currently it is only for AN4 corpus, it has issues however

---
+++ AzharWork/BatchCombine.java
@@ -0,0 +1,186 @@
+package AzharWork;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+import edu.cmu.sphinx.api.*;
+import edu.cmu.sphinx.result.Lattice;
+import edu.cmu.sphinx.result.WordResult;
+
+
+public class BatchCombine {
+
+	static String BaseDir = "/Users/Azhar/Desktop/MDC_Experiments/";
+	static String TestFileIds = "an4/an4_test.fileids";
+	static String BaseWavPath = "/Users/Azhar/Desktop/Exp2/an4/wav/"; 
+	// Define acoustic models
+	static String ModelsPath = BaseDir + "an4/AN4_AM/";
+	static String AcModel0 ="an4_Clean.cd_cont_200";
+	static String AcModel20 ="an4_White20dB.cd_cont_200";
+	static String AcModel15 ="an4_White15dB.cd_cont_200";
+	static String AcModel10 ="an4_White10dB.cd_cont_200";
+	static String[] AcModel = {AcModel0, AcModel20,AcModel15,AcModel10};
+
+	static String DictName = "an4.dic";
+	static final String LM_Name = "an4.lm";
+
+
+	public static void main(String[] args) throws IOException {
+		//WOW, best way to get rid of WARNINGS and info in the log an stop showing them on console
+		//System.err.close();
+
+		// Initialize recognizers, each for specific AM 
+		Configuration config = new Configuration();
+		ConfigSetup conf = new ConfigSetup();
+		StreamSpeechRecognizer CleanRecognizer = conf.BuildRecognizer(ModelsPath , DictName, LM_Name,AcModel[0], config);
+		StreamSpeechRecognizer Recognizer20dB = conf.BuildRecognizer(ModelsPath , DictName, LM_Name,AcModel[1], config);
+		StreamSpeechRecognizer Recognizer15dB = conf.BuildRecognizer(ModelsPath , DictName, LM_Name,AcModel[2], config);
+		StreamSpeechRecognizer Recognizer10dB = conf.BuildRecognizer(ModelsPath , DictName, LM_Name,AcModel[3], config);
+		StreamSpeechRecognizer[]	Recognizer = {CleanRecognizer,Recognizer20dB,Recognizer15dB,Recognizer10dB};
+
+
+		String finalResult = "";
+
+		@SuppressWarnings("resource")
+		Scanner scan = new Scanner(new File(BaseDir + TestFileIds));
+		while(scan.hasNextLine()){
+			String line = scan.nextLine();
+			String fileName = BaseWavPath + line+".sph";
+			System.out.println("Decoding file: " + fileName);
+			//Here you can manipulate the string the way you want
+			Lattice new_lattice = getCombinedResult(Recognizer, fileName);
+			// get textual utterance after combining
+			finalResult= finalResult + getFinalReslts(new_lattice)+"\n";
+
+		}
+
+
+		// Store textual utterances in a file
+		StoreTextResult(finalResult, "Result.txt");
+		System.out.print("Final Result: "+finalResult);    
+
+
+
+
+		System.out.println();
+		System.out.println();
+
+
+
+		// Store lattice
+		System.out.println("Finished ........");
+		// 	lattice10dB.dumpDot("lattice10dB.dot", "lattice10dB.dot");
+		//	lattice15dB.dumpDot("lattice15dB.dot", "lattice15dB.dot");
+		// 	new_lattice.dumpDot("new_Lattice.dot", "New_lattice.dot");
+
+
+
+	}
+
+
+	/**
+	 * 
+	 * @param config
+	 * @param conf
+	 * @param fileName
+	 * @return
+	 * @throws IOException
+	 */
+	private static Lattice getCombinedResult(StreamSpeechRecognizer[] Recognizer, String fileName ) throws IOException {
+
+		// get results for all 4 AMs
+		SpeechResult resultClean = Decode(Recognizer[0],fileName);
+		SpeechResult result20dB = Decode(Recognizer[1],fileName);
+		SpeechResult result15dB = Decode(Recognizer[2],fileName);
+		SpeechResult result10dB = Decode(Recognizer[3],fileName);
+
+		// Gettting lattices results for each AM
+		Lattice latticeClean = new Lattice(resultClean.getResult());
+		Lattice lattice20dB = new Lattice(result20dB.getResult());
+		Lattice lattice15dB = new Lattice(result15dB.getResult());
+		Lattice lattice10dB = new Lattice(result10dB.getResult());
+
+
+		// New combined results
+		Lattice lattice1 = CombineLattice.Combine(latticeClean, lattice20dB);
+		Lattice lattice2 = CombineLattice.Combine(lattice10dB, lattice15dB);
+		Lattice new_lattice = CombineLattice.Combine(lattice1, lattice2);
+		// Compute posteriori, it is required
+		new_lattice.computeNodePosteriors(1.0f); 
+
+		return new_lattice;
+	}
+
+
+	/**
+	 * 
+	 * @param recognizer
+	 * @param speechFilePath
+	 * @return
+	 * @throws IOException
+	 */
+	private static SpeechResult Decode(StreamSpeechRecognizer recognizer, String speechFilePath) throws IOException{
+		// Start recognition
+		recognizer.startRecognition(new FileInputStream(speechFilePath));
+		SpeechResult result = recognizer.getResult();
+		// Stop recognition
+		recognizer.stopRecognition();
+
+		return result;
+
+	}
+
+
+
+	/**
+	 * 
+	 * @param lattice
+	 * @return textual results
+	 */
+	private static String getFinalReslts(Lattice lattice) {
+
+		List<WordResult> words = lattice.getWordResultPath();
+		Iterator<WordResult> i = words.iterator();
+		String FinalResult ="";
+		while(i.hasNext()) {
+			WordResult word = i.next();
+			FinalResult = FinalResult + word.getWord()+" ";
+		}
+
+		return FinalResult;
+	}
+
+	/**
+	 *  
+	 * @param result
+	 * @param fileName
+	 * @return
+	 */
+	private static boolean StoreTextResult(String result, String fileName) {
+
+		boolean done = false;
+
+		// Print to text file
+		try {
+			PrintWriter out = new PrintWriter(fileName);
+			out.println(result);
+			out.flush();
+			out.close();
+			done = true;
+		} catch (FileNotFoundException e) {
+			System.out.println("Error: The output file not found");
+			e.printStackTrace();
+		}
+
+		return done;
+	}
+
+
+
+}
--- AzharWork/LatticeCombination.java
+++ AzharWork/LatticeCombination.java
@@ -1,8 +1,10 @@
 package AzharWork;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
@@ -13,6 +15,7 @@ import edu.cmu.sphinx.api.*;
 import edu.cmu.sphinx.result.Edge;
 import edu.cmu.sphinx.result.Lattice;
 import edu.cmu.sphinx.result.LatticeOptimizer;
+import edu.cmu.sphinx.result.Nbest;
 import edu.cmu.sphinx.result.Node;
 import edu.cmu.sphinx.result.WordResult;
 
@@ -45,10 +48,10 @@ public class LatticeCombination {
       
         Configuration config = new Configuration();
 		ConfigSetup conf = new ConfigSetup();
-		SpeechResult result10dB = recog(config,conf,AcModel[3],speechFilePath);
+		SpeechResult result10dB = recog(config,conf,AcModel[0],speechFilePath);
 		
 			 
-			SpeechResult result15dB = recog(config,conf,AcModel[2],speechFilePath);
+			SpeechResult result15dB = recog(config,conf,AcModel[1],speechFilePath);
 			
 			 
 	      // Gettting lattices results for each AM
@@ -58,32 +61,35 @@ public class LatticeCombination {
 	    	lattice15dB.dumpDot("lattice15dBBeforeUpdate.dot", "lattice15dB.dot");
 	    	
 	    	// Update the AM using the SNR estimator
-//	    	double[] LL_SPS = {-10e7,-10e7};
-//	    	lattice10dB = LatticeTest.UpdateAcousticScore(lattice10dB, LL_SPS[0]);
-//	    	lattice15dB = LatticeTest.UpdateAcousticScore(lattice15dB, LL_SPS[1]);
+	    	double[] LL_SPS = {-1e7,-10e7};
+	    	lattice10dB = LatticeTest.UpdateAcousticScore(lattice10dB, LL_SPS[0]);
+	    	lattice15dB = LatticeTest.UpdateAcousticScore(lattice15dB, LL_SPS[1]);
 	    	
-	    	//Lattice new_lattice = CombineLattice(lattice10dB, lattice15dB, LL_SPS);
-	   // 	Lattice new_lattice = LatticeTest.CombineLattice(lattice10dB, lattice15dB);
-	    	Lattice new_lattice = CombineLattice.Combine(lattice10dB, lattice15dB);
+	    
 	    	
 	    	//Results
 	    	
-	    System.out.println("Speech file Abs Path: " + speechFilePath);
-	        
+	    System.out.println("Speech file Abs Path: " + speechFilePath);  
 	    System.out.println("Reference Utterance: He spoke soothingly.");
-	   
 	    	System.out.println("Result from AM_10dB is:" + result10dB.getResult());
 	    	System.out.println("Result from AM_15dB is:" + result15dB.getResult());
 	    	
 	    	// New combined results
-	    	new_lattice.computeNodePosteriors(1.0f);
-	    	List<WordResult> x = new_lattice.getWordResultPath();
+	    	Lattice new_lattice = CombineLattice.Combine(lattice10dB, lattice15dB);
+	    	new_lattice.computeNodePosteriors(1.0f); 
+	 
+	   // get textual utterance after combining
 	    	
-	    	Iterator<WordResult> i = x.iterator();
-	    	while(i.hasNext()) {
-	    		System.out.println(i.next());
-	    	}
-	    	System.out.println("New result:" + x);
+	   	String finalResult= getFinalReslts(new_lattice);
+	   	finalResult = finalResult+"\n";
+	   	// Store textual utterances in a file
+	   	StoreTextResult(finalResult, "Result.txt");
+   
+    	System.out.print("Final Result: "+finalResult);
+    	System.out.println();
+	System.out.println();
+	
+	    
 	    	
 	    	// Store lattice
 	    	System.out.println("Finished ........");
@@ -94,6 +100,22 @@ public class LatticeCombination {
 			
 			
 		}
+    
+    private static void dumpingResults(Lattice lattice) {
+     	List<Node> xx = lattice.getViterbiPath();
+	    Iterator<Node> nodes = xx.iterator();
+	    
+	    System.out.println();
+	    System.out.print("New results: ");
+	    while(nodes.hasNext()) {
+	    		Node node = nodes.next();
+	    		System.out.print(node.getWord()+" ");
+	    }
+	    	System.out.println();
+	    	
+	     List<WordResult> words = lattice.getWordResultPath();
+	   	Iterator<WordResult> i = words.iterator();
+    }
     
     public static SpeechResult recog(Configuration config,ConfigSetup conf, String AcModel, String speechFilePath) throws IOException{
 
@@ -109,62 +131,49 @@ public class LatticeCombination {
 	
     }
     
-    public static Lattice CombineLattice(Lattice lattice1, Lattice lattice2, double[] LL_SPS){
-    	Lattice fused_lattice = new Lattice();
+  /**
+   * 
+   * @param lattice
+   * @return textual results
+   */
+    private static String getFinalReslts(Lattice lattice) {
     	
-    	// For initial node in the new lattice
-    	Node initialNode = lattice1.getInitialNode();
-    	Node terminalNode = lattice1.getTerminalNode();
-    	
-    	Collection<Edge> edges1 = lattice1.getEdges();
-    	Collection<Node> nodes1 = lattice1.getNodes();
-    	
-    	Collection<Edge> edges2 = lattice2.getEdges();
-    	Collection<Node> nodes2 = lattice2.getNodes();
-    	
-    	fused_lattice.setInitialNode(initialNode);
-    	fused_lattice.setTerminalNode(terminalNode);
-
-    	Iterator<Node> node1_iterator = nodes1.iterator();
-    	Iterator<Edge> edge1_iterator = edges1.iterator();
-    	Iterator<Node> node2_iterator = nodes2.iterator();
-    	Iterator<Edge> edge2_iterator = edges2.iterator();
-
-    	// ignore first node and first edge
-    	Node current_node = node1_iterator.next();
-    	Edge current_edge = edge1_iterator.next();
-    	
-    	while(node1_iterator.hasNext()){
-    	     current_node = node1_iterator.next();
-    		fused_lattice.addNode(current_node.getId(), current_node.getWord().toString(),
-    				+ current_node.getBeginTime(), current_node.getEndTime());
+    	List<WordResult> words = lattice.getWordResultPath();
+    	Iterator<WordResult> i = words.iterator();
+    	String FinalResult ="";
+    	while(i.hasNext()) {
+    		WordResult word = i.next();
+    		FinalResult = FinalResult + word.getWord()+" ";
     	}
     	
-    	while(edge1_iterator.hasNext()){
-    	     current_edge = edge1_iterator.next();
-    		double AC_score = LL_SPS[0]+current_edge.getAcousticScore();
-    		
-    		fused_lattice.addEdge(current_edge.getFromNode(), current_edge.getToNode(), AC_score, current_edge.getLMScore());
-    	}
-	
-    	// ignore first node and first edge
-    	current_node = node2_iterator.next();
-    current_edge = edge1_iterator.next();
-    	
-    	while(node2_iterator.hasNext()){
-    	    current_node = node2_iterator.next();
-    		fused_lattice.addNode(current_node.getId(), current_node.getWord().toString(), current_node.getBeginTime(), current_node.getEndTime());
-    	}
-    	while(edge2_iterator.hasNext()){
-    		 current_edge = edge2_iterator.next();
-    		double AC_score = LL_SPS[1]+current_edge.getAcousticScore();
-    		
-    		fused_lattice.addEdge(current_edge.getFromNode(), current_edge.getToNode(), AC_score, current_edge.getLMScore());
-    	}
-    
-    return fused_lattice;	
-
+    	return FinalResult;
     }
     
+   /**
+    *  
+    * @param result
+    * @param fileName
+    * @return
+    */
+    private static boolean StoreTextResult(String result, String fileName) {
+        	
+    boolean done = false;
+     
+    	// Print to text file
+    	try {
+			PrintWriter out = new PrintWriter(fileName);
+			out.println(result);
+			out.flush();
+			out.close();
+			done = true;
+		} catch (FileNotFoundException e) {
+			System.out.println("Error: The output file not found");
+			e.printStackTrace();
+		}
+
+    	 return done;
+    }
+    
+       
          
 }
